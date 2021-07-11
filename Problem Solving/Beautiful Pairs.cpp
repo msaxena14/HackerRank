0664edb8de04b073cce43dfbb40a02e2f8b1f72d
Madhavam Saxena <68888933+msaxena14@users.noreply.github.com>
2021-07-11 17:49:41 +0530

    Create Beautiful Pairs.cpp

---
+++ Problem Solving/Beautiful Pairs.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'beautifulPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY A
+ *  2. INTEGER_ARRAY B
+ */
+
+int beautifulPairs(vector<int> A, vector<int> B) {
+    multiset<int> m1,m2;
+    int count = 0;
+    for(auto ele : A){
+        m1.insert(ele);
+    }
+    for(auto ele : B){
+        m2.insert(ele);
+    }
+    for(auto &it : A){
+        auto itr = m2.find(it);
+        if(itr != m2.end()){
+            ++count;
+            m2.erase(itr);
+        }
+    }
+    if(m2.size() == 0)
+        --count;
+    else
+        ++count;
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string A_temp_temp;
+    getline(cin, A_temp_temp);
+
+    vector<string> A_temp = split(rtrim(A_temp_temp));
+
+    vector<int> A(n);
+
+    for (int i = 0; i < n; i++) {
+        int A_item = stoi(A_temp[i]);
+
+        A[i] = A_item;
+    }
+
+    string B_temp_temp;
+    getline(cin, B_temp_temp);
+
+    vector<string> B_temp = split(rtrim(B_temp_temp));
+
+    vector<int> B(n);
+
+    for (int i = 0; i < n; i++) {
+        int B_item = stoi(B_temp[i]);
+
+        B[i] = B_item;
+    }
+
+    int result = beautifulPairs(A, B);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
